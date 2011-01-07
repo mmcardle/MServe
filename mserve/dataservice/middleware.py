52ab5dbcfbdcd28e955fb877dcb8faf9e8c0b290
Mark McArdle <mm@it-innovation.soton.ac.uk>
2011-01-07 14:24:01 +0000

    Fix to middleware for no REQUEST_URI

--- mserve/dataservice/middleware.py
+++ mserve/dataservice/middleware.py
@@ -5,27 +5,28 @@ import usage_store as usage_store
 class AuthMiddleware(object):
 
     def process_response(self, request, response):
-        uri = request.META['REQUEST_URI']
-        if uri.startswith("/mfileapi/get/"):
-            match = re.search("[\w]{8}(-[\w]{4}){3}-[\w]{12}", uri)
-            # No match
-            if match is not None:
-                mfileid = match.group(0)
-                logging.info("Match %s" % mfileid)
-                usage_store.stoprecording(mfileid,usage_store.metric_responsetime)
+        if request.META.has_key('REQUEST_URI'):
+            uri = request.META['REQUEST_URI']
+            if uri.startswith("/mfileapi/get/"):
+                match = re.search("[\w]{8}(-[\w]{4}){3}-[\w]{12}", uri)
+                # No match
+                if match is not None:
+                    mfileid = match.group(0)
+                    logging.info("Match %s" % mfileid)
+                    usage_store.stoprecording(mfileid,usage_store.metric_responsetime)
 
         return response
 
     def process_request(self, request):
-
-        uri = request.META['REQUEST_URI']
-        if uri.startswith("/mfileapi/get/"):
-            match = re.search("[\w]{8}(-[\w]{4}){3}-[\w]{12}", uri)
-            # No match
-            if match is not None:
-                mfileid = match.group(0)
-                logging.info("Match %s" % mfileid)
-                usage_store.startrecording(mfileid,usage_store.metric_responsetime,1)
+        if request.META.has_key('REQUEST_URI'):
+            uri = request.META['REQUEST_URI']
+            if uri.startswith("/mfileapi/get/"):
+                match = re.search("[\w]{8}(-[\w]{4}){3}-[\w]{12}", uri)
+                # No match
+                if match is not None:
+                  mfileid = match.group(0)
+                  logging.info("Match %s" % mfileid)
+                  usage_store.startrecording(mfileid,usage_store.metric_responsetime,1)
 
         return
 
