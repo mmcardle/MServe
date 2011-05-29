c6cb6b232ff8a62d23ad534a757545207317fdcf
Mark McArdle <mm@it-innovation.soton.ac.uk>
2011-05-29 12:39:00 +0100

    added celery concurrency

--- mserve/celeryd.sh
+++ mserve/celeryd.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Running Celery Daemon..."
-sudo -u www-data ./manage.py celeryd --verbosity=2 --logfile=/var/mserve/mserve.log --loglevel=DEBUG -E
+sudo -u www-data ./manage.py celeryd --concurrency=10 --verbosity=2 --logfile=/var/mserve/mserve.log --loglevel=DEBUG -E
