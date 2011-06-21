577c0b567902e718725a34871ad46dc88e89e590
Mark McArdle <mm@it-innovation.soton.ac.uk>
2011-06-21 15:36:26 +0100

    celeryd.sh update to use multi_start

--- mserve/celeryd.sh
+++ mserve/celeryd.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "Running Celery Daemon..."
-#sudo -u www-data ./manage.py celeryd --concurrency=10 --verbosity=2 --logfile=/var/mserve/mserve.log --loglevel=DEBUG -E
-sudo -u www-data ./manage.py celeryd --concurrency=10 --verbosity=2 --loglevel=DEBUG -E
+echo "Restarting Celery Daemon..."
+hn=$HOSTNAME
+sudo -u www-data ./manage.py celeryd_multi restart --logfile=/var/mserve/celeryd%n.log -l DEBUG 2 -n:1 local.$hn -n:2 remote.$hn -Q:1 local_tasks -Q:2 remote_tasks -c 5 --pidfile=/var/mserve/celeryd%n.pid
