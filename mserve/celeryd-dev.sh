d26eab0ab7cefd3095ad338d47f1add3e5860dfd
Mark McArdle <mm@it-innovation.soton.ac.uk>
2011-09-13 14:16:47 +0100

    Allow Event reports from celery

--- mserve/celeryd-dev.sh
+++ mserve/celeryd-dev.sh
@@ -8,4 +8,4 @@ MSERVE_HOME=/home/mm/dev/pp-dataservice/mserve/
 MSERVE_DATA=/home/mm/mserve-test-data
 MSERVE_LOG=/home/mm/dev/pp-dataservice/mserve/
 hn=$HOSTNAME
-${MSERVE_HOME}/manage.py celeryd_multi ${COMMAND} --logfile=${MSERVE_LOG}/celeryd%n.log -l DEBUG 2 -n:1 normal.$hn -n:2 priority.$hn -Q:1 normal_tasks -Q:2 priority_tasks -c 5 --pidfile=${MSERVE_DATA}/celeryd%n.pid
+${MSERVE_HOME}/manage.py celeryd_multi ${COMMAND} -E --logfile=${MSERVE_LOG}/celeryd%n.log -l DEBUG 2 -n:1 normal.$hn -n:2 priority.$hn -Q:1 normal_tasks -Q:2 priority_tasks -c 5 --pidfile=${MSERVE_DATA}/celeryd%n.pid
